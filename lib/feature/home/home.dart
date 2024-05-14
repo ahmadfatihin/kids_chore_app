5bef50d7fb9e87ea49a2bd3cfb3120e99c6a9a97
gifhary <gifhary.1997@gmail.com>
2024-05-14 16:56:15 +0700

    a buch of more code

--- lib/feature/home/home.dart
+++ lib/feature/home/home.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/intl.dart';
 
 class Home extends StatelessWidget {
@@ -61,6 +65,146 @@ class ParentHome extends StatelessWidget {
             DateFormat('hh:mm').format(DateTime.now()),
             style: const TextStyle(fontSize: 64),
           ),
+          const SizedBox(height: 70),
+          AspectRatio(
+            aspectRatio: 366 / 140,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(255, 242, 194, 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Flexible(
+                          child: Text(
+                            'Make a daily schedule with your child',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromRGBO(155, 195, 255, 1),
+                          ),
+                          child: const Text(
+                            'Create',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset('assets/images/calendar.png')
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          Row(
+            children: [
+              Flexible(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(255, 226, 230, 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/family.png'),
+                      SizedBox(width: 10),
+                      Flexible(
+                        child: Text(
+                          'Manage your family',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 15),
+              Flexible(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(228, 226, 255, 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/podium.png'),
+                      SizedBox(width: 10),
+                      Flexible(
+                        child: Text(
+                          'Track child progress',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+          Row(
+            children: [
+              Flexible(
+                flex: 5,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(255, 242, 194, 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/gift.png'),
+                      SizedBox(width: 10),
+                      Flexible(
+                        child: Text(
+                          'Manage Rewards',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 15),
+              Flexible(
+                flex: 3,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(255, 226, 230, 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset('assets/images/clock.png'),
+                      SizedBox(width: 10),
+                      Flexible(
+                        child: Text(
+                          'History',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
