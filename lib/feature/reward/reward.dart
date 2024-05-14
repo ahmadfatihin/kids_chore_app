fdf5284db8cad7049e366518224d9db91649f7b9
gifhary <gifhary.1997@gmail.com>
2024-05-14 17:42:10 +0700

    more

--- lib/feature/reward/reward.dart
+++ lib/feature/reward/reward.dart
@@ -15,6 +15,11 @@ class RewardScreen extends StatelessWidget {
       'Steam Wallet'
     ];
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color.fromRGBO(155, 195, 255, 1),
+        child: Icon(Icons.add),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -31,22 +36,23 @@ class RewardScreen extends StatelessWidget {
                 ListView(
                   shrinkWrap: true,
                   children: List.generate(
-                      list.length,
-                      (index) => Container(
-                            margin: EdgeInsets.only(bottom: 10),
-                            padding: EdgeInsets.all(15),
-                            decoration: BoxDecoration(
-                              color: Color.fromRGBO(255, 242, 194, 0.7),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Row(
-                              children: [
-                                Text('${index + 1}.'),
-                                SizedBox(width: 10),
-                                Text(list[index]),
-                              ],
-                            ),
-                          )),
+                    list.length,
+                    (index) => Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 242, 194, 0.7),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        children: [
+                          Text('${index + 1}.'),
+                          SizedBox(width: 10),
+                          Text(list[index]),
+                        ],
+                      ),
+                    ),
+                  ),
                 )
               ],
             ),
