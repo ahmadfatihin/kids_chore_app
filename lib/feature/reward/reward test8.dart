25cab46537809972f46137c0c3483973dbdd19c5
Fatih <ahmdfatihin17@gmail.com>
2024-10-28 16:00:02 +0700

    feat: improve output

--- lib/feature/reward/reward test8.dart
+++ lib/feature/reward/reward test8.dart
@@ -10,6 +10,7 @@ class RewardScreen extends StatelessWidget {
       'Playground Ticket',
       'Money - 100 USD',
       'Money - 50 USD',
+      'Money - 50 USD',
       'Snack',
       'Company stock 1000 units',
       'Steam Wallet'
